0247a9a22b5f35aca451d8e822e8a4d8117bcced
Masha1986 <9312613800@mail.ru>
2022-02-24 11:04:30 +0300

    Change PostmanEchoTest

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
 
                 .then()
-                .statusCode(200)
+                .statusCode(3Postman00)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
                 .contentType(ContentType.JSON)
                 .body("data", equalTo("some data"));
